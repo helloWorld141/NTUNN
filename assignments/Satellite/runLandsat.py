cdeb8240a0d04f286cd3b3162fea754d35ef82d7
Kyle Nguyen <nghia002@outlook.com>
2018-10-11 19:41:50 +0800

    change in runLandsat

--- assignments/Satellite/runLandsat.py
+++ assignments/Satellite/runLandsat.py
@@ -3,10 +3,16 @@ with open("log", 'w') as f: #clear log file
     pass
 batch_size_search_space = [4, 8, 16, 32, 64]
 for batch_size in batch_size_search_space[::-1]:
-    conf = json.loads(open('conf').read())
+    file = open('conf', 'w')
+    conf = file.read()
+    if len(conf) != 0:
+        conf = json.loads(conf)
+    else:
+        conf = {}
+    # file.close()
     conf['batch_size'] = batch_size
-    with open('conf', 'w') as f:
-        f.write(conf)
+    json.dump(conf, file, ensure_ascii=False)
+    file.close()
 #plot running time curves
 # plt.figure("Running time")
 # plt.plot(range(epochs), running_time, label=name)
